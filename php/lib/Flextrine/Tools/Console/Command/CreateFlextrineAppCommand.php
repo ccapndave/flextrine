e8c20a316561edb6a4e9ddd66de973feb6ec3b7d
Dave Keen <dev@ruffness.com>
2011-02-24 18:37:22 +0100

    Updated project create command

--- php/lib/Flextrine/Tools/Console/Command/CreateFlextrineAppCommand.php
+++ php/lib/Flextrine/Tools/Console/Command/CreateFlextrineAppCommand.php
@@ -1,4 +1,25 @@
 <?php
+/**
+ * Copyright 2011 Dave Keen
+ * http://www.actionscriptdeveloper.co.uk
+ * 
+ * This file is part of Flextrine.
+ * 
+ * Flextrine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * and the Lesser GNU General Public License along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 
 namespace Flextrine\Tools\Console\Command;
 
@@ -6,7 +27,7 @@ use Symfony\Component\Console\Input\InputArgument,
 	Symfony\Component\Console\Input\InputOption,
 	Symfony\Component\Console,
 	Flextrine\Tools\AS3EntityGenerator,
-	Flextrine\Tools\FlextrineProjectGenerator,
+	Flextrine\Tools\FlextrineAppGenerator,
 	Doctrine\ORM\Tools\Console\MetadataFilter,
 	Doctrine\ORM\Tools\DisconnectedClassMetadataFactory;
 
@@ -37,15 +58,15 @@ EOT
 	protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output) {
 		$em = $this->getHelper('em')->getEntityManager();
 
-		$projectName = $input->getArgument('project-name');
+		$appName = $input->getArgument('app-name');
 
-		$destPath = ROOT_PATH.DIRECTORY_SEPARATOR."app".DIRECTORY_SEPARATOR.basename($projectName);
+		$destPath = ROOT_PATH.DIRECTORY_SEPARATOR."app".DIRECTORY_SEPARATOR.basename($appName);
 		
 		if (file_exists($destPath)) {
 			$output->write("This app already exists.  You must delete it manually if you want to regenerate it.");
 		} else {
-			$flextrineProjectGenerator = new FlextrineProjectGenerator($em);
-			$flextrineProjectGenerator->generate($projectName, $destPath);
+			$flextrineAppGenerator = new FlextrineAppGenerator($em);
+			$flextrineAppGenerator->generate($appName, $destPath);
 
 			$output->write("New Flextrine application created successfully in $destPath.", true);
 			$output->write("", true);
